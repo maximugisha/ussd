f921b0d0e1b64c280e08b91094b37480b195d15d
maximugisha <maximugisha@gmail.com>
2020-04-27 12:08:13 +0300

    integrating sms into ussd

--- webApp.py
+++ webApp.py
@@ -1,49 +1,27 @@
 from __future__ import print_function
-import os
+
 from flask import Flask, request
 from flask_restful import Api
 
 import africastalking
+
 app = Flask(__name__)
 api = Api(app)
 
-username = os.getenv('user_name', 'sandbox')
-api_key = os.getenv('6f0e59d192c4940953563de43826d9e9a91528400b70b06fad714d7f99337734', 'fake')
+# Create your credentials
+username = "sandbox"
+apikey = "6f0e59d192c4940953563de43826d9e9a91528400b70b06fad714d7f99337734"
 
-africastalking.initialize(username, api_key)
+# Initialize the SDK
+africastalking.initialize(username, apikey)
+
+# Get the SMS service
 sms = africastalking.SMS
-airtime = africastalking.Airtime
-payment = africastalking.Payment
 
-class SMS:
-    def __init__(self):
-        # Set your app credentials
-        self.username = "sandbox"
-        self.api_key = "0fd2d64f27622921dba1fc7204073dfaa08be88eac31cae1f683b39f194d0d5b"
-        # Initialize the SDK
-        africastalking.initialize(self.username, self.api_key)
-        # Get the SMS service
-        self.sms = africastalking.SMS
-
-
-    def send(self):
-        # Set the numbers you want to send to in international format
-        recipients = ["+256702431725"]
-
-        # Set your message
-        message = "I'm a lumberjack and it's ok, I sleep all night and I work all day";
-
-        # Set your shortCode or senderId
-        sender = "14262"
-        try:
-            # Thats it, hit send and we'll take care of the rest.
-            response = self.sms.send(message, recipients, sender)
-            print(response)
-        except Exception as e:
-            print('Encountered an error while sending: %s' % str(e))
-
-if __name__ == '__main__':
-    SMS().send()
+# Define some options that we will use to send the SMS
+recipients = ['YOUR_PHONE_NUMBER_GOES_HERE']
+message = 'I\'m a lumberjack and its ok, I sleep all night and I work all day'
+sender = '14262'
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -68,10 +46,14 @@ def ussd():
         response = "END Your phone number is " + phone_number
 
     elif text == '3':
-        if SMS.send:
-            response = "END SMS Received"
-        else:
-            response = "END FAILED"
+        # Send the SMS
+        try:
+            # Once this is done, that's it! We'll handle the rest
+            response = sms.send(message, recipients, sender)
+            print(response)
+        except Exception as e:
+            print(f"Houston, we have a problem {e}")
+
 
     elif text == '1*1':
         accountNumber = "ACC1001"
