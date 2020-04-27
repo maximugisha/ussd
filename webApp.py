cf3540b3881b11f507c0057a89516e0e2e8564d1
maximugisha <maximugisha@gmail.com>
2020-04-27 15:52:35 +0300

    sms alone

--- webApp.py
+++ webApp.py
@@ -3,39 +3,32 @@ from __future__ import print_function
 from flask import Flask, request
 from flask_restful import Api
 
-import africastalking
-
 app = Flask(__name__)
 api = Api(app)
 
 
-class SMS:
-    def __init__(self):
-        # Set your app credentials
-        self.username = "sandbox"
-        self.api_key = "1578499043ae12ca0f0a9e00070d4ac9cde89ce0a36ee5d7f87bb47011a671ae"
+#Import the AfricasTalking SDK into your app
+import africastalking
 
-        # Initialize the SDK
-        africastalking.initialize(self.username, self.api_key)
+#Create your credentials
+username = "sandbox"
+apikey = "1578499043ae12ca0f0a9e00070d4ac9cde89ce0a36ee5d7f87bb47011a671ae"
 
-        # Get the SMS service
-        self.sms = africastalking.SMS
+#Initialize the SDK
+africastalking.initialize(username, apikey)
 
-def send(self):
-    # Set the numbers you want to send to in international format
-    recipients = ["+256702431725", "14262"]
+#Get the SMS service
+sms = africastalking.SMS
 
-    # Set your message
-    message = "I'm a lumberjack and it's ok, I sleep all night and I work all day"
+#Define some options that we will use to send the SMS
+recipients = ['+256702431725']
+message = 'I\'m a erflumberjack and its ok, I sleep all night and I work all day'
+sender = '14262'
 
-    # Set your shortCode or senderId
-    sender = "14262"
-    try:
-        # Thats it, hit send and we'll take care of the rest.
-        response = self.sms.send(message, recipients, sender)
-        print(response)
-    except Exception as e:
-        print('Encountered an error while sending: %s' % str(e))
-
-if __name__ == '__main__':
-    SMS().send()
+#Send the SMS
+try:
+    #Once this is done, that's it! We'll handle the rest
+    response = sms.send(message, recipients, sender)
+    print(response)
+except Exception as e:
+    print(f"Houston, we have a problem {e}")
