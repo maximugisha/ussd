40314f841384a056247a78a559594e722a3d8066
maximugisha <maximugisha@gmail.com>
2020-04-27 16:41:49 +0300

    sms alone finished successifuly

--- webApp.py
+++ webApp.py
@@ -2,13 +2,12 @@ from __future__ import print_function
 
 from flask import Flask
 from flask_restful import Api
-#Import the AfricasTalking SDK into your app
 import africastalking
 
 app = Flask(__name__)
 api = Api(app)
 
-#Create your credentials
+# Create your credentials
 username = "sandbox"
 apikey = "1578499043ae12ca0f0a9e00070d4ac9cde89ce0a36ee5d7f87bb47011a671ae"
 
@@ -18,20 +17,18 @@ africastalking.initialize(username, apikey)
 # Get the SMS service
 sms = africastalking.SMS
 
-@app.route('/',methods=['GET', 'POST'])
-def sendMessage():
-    #Define some options that we will use to send the SMS
+@app.route('/', methods=['GET', 'POST'])
+def sendsms():
+    # Define some options that we will use to send the SMS
     recipients = ['+256702431725', '+256775097505']
-    message = 'I\'m a erflumberjack and its ok, I sleep all night and I work all day'
+    message = 'I want to make an order'
     sender = '14262'
 
-    #Send the SMS
+    # Send the SMS
     try:
-        #Once this is done, that's it! We'll handle the rest
+        # Once this is done, that's it! We'll handle the rest
         response = sms.send(message, recipients, sender)
         print(response)
     except Exception as e:
         print(f"Houston, we have a problem {e}")
-
-
-sendMessage()
+    return response
